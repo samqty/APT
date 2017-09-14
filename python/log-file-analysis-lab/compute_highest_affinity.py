7e27c068b948becff91ab73c3b14b5c5c1f0e6f1
Samuel Cherinet <scherinet@VISIONWEB.COM>
2017-09-14 10:28:03 -0500

    completed assignment code for WH2

--- python/log-file-analysis-lab/compute_highest_affinity.py
+++ python/log-file-analysis-lab/compute_highest_affinity.py
@@ -1,4 +1,3 @@
-
 # No need to process files and manipulate strings - we will
 # pass in lists (of equal length) that correspond to 
 # sites views. The first list is the site visited, the second is
@@ -7,7 +6,32 @@
 # See the test cases for more details.
 
 def highest_affinity(site_list, user_list, time_list):
-  # Returned string pair should be ordered by dictionary order
-  # I.e., if the highest affinity pair is "foo" and "bar"
-  # return ("bar", "foo"). 
-  return ('abc', 'def')
+    # Returned string pair should be ordered by dictionary order
+    # I.e., if the highest affinity pair is "foo" and "bar"
+    # return ("bar", "foo").
+    visitlog = dict()
+
+    for i in range(0, len(site_list)):
+        if user_list[i] in visitlog:
+            if site_list[i] in visitlog[user_list[i]]:
+                pass
+            else:
+                visitlog[user_list[i]].append(site_list[i])
+        else:
+            visitlog[user_list[i]] = [site_list[i]]
+
+    affinitylog = dict()
+    for v in visitlog:
+        for site1 in visitlog[v]:
+            for site2 in visitlog[v]:
+                if site1 != site2:
+                    if site1 + "-" + site2 in affinitylog:
+                        affinitylog[site1 + "-" + site2] += 1
+                    else:
+                        affinitylog[site1 + "-" + site2] = 1
+                else:
+                    pass
+
+    bestaffinity = sorted(sorted(affinitylog, key=affinitylog.get, reverse=True)[0].split("-"))
+
+    return (bestaffinity[0], bestaffinity[1])
--- python/scraping-lab/yahoo_options_data.py
+++ python/scraping-lab/yahoo_options_data.py
@@ -2,7 +2,50 @@ import json
 import sys
 import re
 import urllib 
+from bs4 import BeautifulSoup
+import json
+import re
 
 def contractAsJson(filename):
-  jsonQuoteData = "[]"
-  return jsonQuoteData
+
+  with open(filename) as fp:
+      soup = BeautifulSoup(fp)
+
+  currPrice = float(soup.find_all(class_='time_rtq_ticker')[0].span.text)
+  dateUrls = []
+  optionQuotes = []
+
+  dateUrlTags = soup.select('#yfncsumtab')[0].select('a[href*=m=]')
+  for d in dateUrlTags :
+    dateUrls.append('http://finance.yahoo.com'+d['href'].replace('&','&amp;'))
+
+  rows = soup.select('.yfnc_datamodoutline1 > tr > td > table > tr')
+  for row in rows :
+    cells = row.select('td')
+    if len(cells) > 0:
+      p = re.compile('(?P<symboldate>[A-Z]+\d+)(?P<type>[A-Z]+)\d+')
+      m = p.search(cells[1].text)
+      
+      change = cells[3].text
+      if len(cells[3].select('img[alt=Up]')) > 0 :
+        change = change.replace(' ','+')
+      elif len(cells[3].select('img[alt=Down]')) > 0:
+        change = change.replace(' ', '-')
+
+      optionQuotes.append({
+        'Strike': cells[0].text,
+        'Symbol': m.group('symboldate')[0:len(m.group('symboldate'))-6],
+        'Type': m.group('type'),
+        'Date': m.group('symboldate')[len(m.group('symboldate'))-6:len(m.group('symboldate'))],
+        'Last': cells[2].text,
+        'Change': change,
+        'Bid': cells[4].text,
+        'Ask': cells[5].text,
+        'Vol': cells[6].text,
+        'Open': cells[7].text
+        })
+      
+      optionQuotes = sorted(optionQuotes,key=lambda x: float(x['Open'].replace(',','')),reverse=True)
+
+  return json.dumps({'currPrice':currPrice,'dateUrls':dateUrls,'optionQuotes':optionQuotes},indent=4,sort_keys=True,separators=(',',': '))
+
